5eca53c6a6d882b8d075afc51daad0a729da26b8
Adoyo Clifford <adoyoclifford@gmail.com>
2023-05-22 07:44:06 +0300

    initial commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -9,7 +9,7 @@ public class VersioningPersonController {
 
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
-        return new PersonV1("Boob Charlie");
+        return new PersonV1("Bob Charlie");
     }
 
     @GetMapping(path = "/v2/person")
@@ -17,13 +17,23 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Bob","Charlie"));
     }
 
-    @GetMapping(path = "/person",params = "version = 1")
+    @GetMapping(path = "/person",params = "version=1")
     public PersonV1 getFirstVersionOfPersonRequestParameters() {
-        return new PersonV1("Boob Charlie");
+        return new PersonV1("Bob Charlie");
     }
 
     @GetMapping(path = "/person", params = "version=2")
     public PersonV2 getSecondVersionOfPersonRequestParameters() {
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeaders() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", params = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeaders() {
         return new PersonV2(new Name("Bob","Charlie"));
     }
 }
