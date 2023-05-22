8a894dcb5cb950d7000433ede26c9791b0151866
Adoyo Clifford <adoyoclifford@gmail.com>
2023-05-22 08:16:25 +0300

    initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,27 @@
+package com.in28minutes.rest.webservices.restfulwebservices.filtering;
+
+public class SomeBean {
+    String filed1;
+    String filed2;
+    String filed3;
+
+    public SomeBean(String filed1, String filed2, String filed3) {
+        this.filed1 = filed1;
+        this.filed2 = filed2;
+        this.filed3 = filed3;
+    }
+
+    public String getFiled1() {
+        return filed1;
+    }
+
+    public String getFiled2() {
+        return filed2;
+    }
+
+    public String getFiled3() {
+        return filed3;
+    }
+
+
+}
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import jakarta.validation.Valid;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -24,19 +27,19 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.user(id);
         if (user == null)
             throw new UserNotFoundException("id:" + id);
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all_users"));
+        return entityModel;
     }
 
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id) {
         service.deleteUserById(id);
-//        if (user == null)
-//            throw new UserNotFoundException("id:" + id);
-//        return user;
     }
 
     @PostMapping("/users")
